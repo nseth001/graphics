4afa756a80c0df6f4c1502bfc987edee85b10605
nseth001 <nseth001@ucr.edu>
2018-01-27 18:30:12 -0800

    fixed if conditional in render_world

--- proj-rt-files/src/render_world.cpp
+++ proj-rt-files/src/render_world.cpp
@@ -1,96 +1,3 @@
-// #include <vector>
-// #include "render_world.h"
-// #include "flat_shader.h"
-// #include "object.h"
-// #include "light.h"
-// #include "ray.h"
-//
-//
-// Render_World::Render_World()
-//     :background_shader(0),ambient_intensity(0),enable_shadows(true),
-//     recursion_depth_limit(3),disable_fresnel_reflection(false),disable_fresnel_refraction(false)
-// {}
-//
-// Render_World::~Render_World()
-// {
-//     delete background_shader;
-//     for(size_t i=0;i<objects.size();i++) delete objects[i];
-//     for(size_t i=0;i<lights.size();i++) delete lights[i];
-// }
-//
-// // Find the closest object of intersection and return the object that was
-// // intersected.  Record the Hit structure in hit.  If no intersection occurred,
-// // return NULL.  Note that in the case of a Boolean, the object returned will be
-// // the Boolean, but the object stored in hit will be the underlying primitive.
-// // Any intersection with t<=small_t should be ignored.
-// Object* Render_World::Closest_Intersection(const Ray& ray,Hit& hit)
-// {
-//     // TODO
-//     double min_t = std::numeric_limits<double>::max();
-//
-//     Object* closest_object = NULL;
-//
-//     for (Object* object: objects) {
-//         std::vector<Hit> hits_list;
-//
-//         object->Intersection(ray, hits_list);
-//
-//         for(double i = 0; i < hits_list.size(); i++) {
-//             if (hits_list[i].t > small_t && hits_list[i].t < min_t)
-//             {
-//                 hit = hits_list[i];
-//                 closest_object = object;
-//             }
-//         }
-//
-//     }
-//
-//     return closest_object;
-// }
-//
-// // set up the initial view ray and call
-// void Render_World::Render_Pixel(const ivec2& pixel_index)
-// {
-//     //Ray ray; // TODO: set up the initial view ray here
-//     vec3 endpoint = camera.position;
-//     vec3 worldpos = camera.World_Position(pixel_index);
-//     //vec3 direction = (worldpos - endpoint).normalized();
-//     //ray function already normalizes inputs.
-//     Ray ray(endpoint, (worldpos-endpoint));
-//     vec3 color=Cast_Ray(ray,1);
-//     camera.Set_Pixel(pixel_index,Pixel_Color(color));
-// }
-// void Render_World::Render()
-// {
-//     for(int j=0;j<camera.number_pixels[1];j++)
-//         for(int i=0;i<camera.number_pixels[0];i++)
-//             Render_Pixel(ivec2(i,j));
-// }
-//
-// // cast ray and return the color of the closest intersected surface point,
-// // or the background color if there is no object intersection
-// vec3 Render_World::Cast_Ray(const Ray& ray,int recursion_depth)
-// {
-//     // TODO
-//     vec3 color, dummy;
-//
-//     Hit h;
-//     Object* obj = Closest_Intersection(ray, h);
-//
-//     if (obj != nullptr){
-//         color = obj->material_shader-> Shade_Surface(ray, ray.Point(h.t), obj->Normal(ray.Point(h.t)), recursion_depth, h.ray_exiting);
-//     }
-//
-//     else {
-//         color = background_shader->Shade_Surface(ray, dummy, dummy, recursion_depth, false);
-//     }
-//
-//
-//
-//     // determine the color here
-//
-//     return color;
-// }
 #include <vector>
 #include "render_world.h"
 #include "flat_shader.h"
@@ -118,42 +25,43 @@ Render_World::~Render_World()
 // Any intersection with t<=small_t should be ignored.
 Object* Render_World::Closest_Intersection(const Ray& ray,Hit& hit)
 {
-	 Object* closest_object = NULL;
-
     // TODO
     double min_t = std::numeric_limits<double>::max();
-    // ray.Intersection(ray, hit);
-	for(Object* object : objects)
-	{
-		std::vector<Hit> v_hit;
 
-		if(object ->Intersection(ray, v_hit)){
-		    for(unsigned int i = 0; i < v_hit.size(); i++) {
-		        if (v_hit.at(i).t < min_t && v_hit.at(i).t > small_t) {
-		            hit = v_hit.at(i);
-		            closest_object = object;
-                    min_t = v_hit.at(i).t;
-		        }
-		    }
-		}
-	}
+    Object* closest_object = NULL;
+
+    for (Object* object: objects) {
+        std::vector<Hit> hits_list;
+
+        if(object->Intersection(ray, hits_list)) {
+
+          for(double i = 0; i < hits_list.size(); i++) {
+              if (hits_list[i].t > small_t && hits_list[i].t < min_t)
+              {
+                  hit = hits_list[i];
+                  closest_object = object;
+                  min_t = hits_list.at(i).t;
+            }
+        }
+      }
+
+    }
 
     return closest_object;
-
-
 }
 
 // set up the initial view ray and call
 void Render_World::Render_Pixel(const ivec2& pixel_index)
 {
-    //Ray m_ray; // TODO: set up the initial view ray here
-
-    Ray ray(camera.position, (camera.World_Position(pixel_index) - camera.position));
-
+    //Ray ray; // TODO: set up the initial view ray here
+    vec3 endpoint = camera.position;
+    vec3 worldpos = camera.World_Position(pixel_index);
+    //vec3 direction = (worldpos - endpoint).normalized();
+    //ray function already normalizes inputs.
+    Ray ray(endpoint, (worldpos-endpoint));
     vec3 color=Cast_Ray(ray,1);
     camera.Set_Pixel(pixel_index,Pixel_Color(color));
 }
-
 void Render_World::Render()
 {
     for(int j=0;j<camera.number_pixels[1];j++)
@@ -167,20 +75,21 @@ vec3 Render_World::Cast_Ray(const Ray& ray,int recursion_depth)
 {
     // TODO
     vec3 color, dummy;
-    vec3 intersect, normal;
-    Hit m_hit;
-    Object* m_object = Closest_Intersection(ray, m_hit);
 
-    if(m_object != NULL) {
-        intersect = ray.Point(m_hit.t);
-        normal = m_object->Normal(intersect);
-        color = m_object->material_shader->Shade_Surface(ray, intersect, normal, recursion_depth, false);
+    Hit h;
+    Object* obj = Closest_Intersection(ray, h);
+
+    if (obj != nullptr){
+        color = obj->material_shader-> Shade_Surface(ray, ray.Point(h.t), obj->Normal(ray.Point(h.t)), recursion_depth, h.ray_exiting);
     }
+
     else {
         color = background_shader->Shade_Surface(ray, dummy, dummy, recursion_depth, false);
     }
-    // determine the color here
 
+
+
+    // determine the color here
 
     return color;
 }
--- proj-rt-files/src/sphere.cpp
+++ proj-rt-files/src/sphere.cpp
@@ -1,59 +1,3 @@
-// #include "sphere.h"
-// #include "ray.h"
-//
-//
-// // Determine if the ray intersects with the sphere
-// bool Sphere::Intersection(const Ray& ray, std::vector<Hit>& hits) const
-// {
-//     // TODO
-//     vec3 u;
-//     vec3 v;
-//     vec3 w;
-//     vec3 center;
-//     double a, b, c, radius;
-//     //finish defs
-//     center = this->center;
-//     u = ray.direction;
-//     w = ray.endpoint;
-//     v = w - center;
-//
-//     radius = this->radius;
-//
-//     a = dot(u,u);
-//     b = dot(u,v);
-//     c = dot(v,v) - radius*radius;
-//
-//     double discr = (b*b) - c;
-//
-//
-//     if (discr > 0) {
-//         double t1 = (-b - pow(discr, 0.5));
-//         double t2 = (-b + pow(discr, 0.5));
-//
-//         if (t1 > 0) {
-//             Hit hit1 = {this, t1, true};
-//             hits.push_back(hit1);
-//         }
-//
-//         if (t2 > 0) {
-//             Hit hit2 = {this, t2, true};
-//             hits.push_back(hit2);
-//         }
-//         return true;
-//     }
-//     return false;
-// }
-//
-// vec3 Sphere::Normal(const vec3& point) const
-// {
-//     vec3 normal;
-//     // TODO: set the normal
-//     //normal = point - center;
-//     //normal.normalized()
-//     normal = (point - center).normalized();
-//     return normal;
-// }
-
 #include "sphere.h"
 #include "ray.h"
 
@@ -62,59 +6,50 @@
 bool Sphere::Intersection(const Ray& ray, std::vector<Hit>& hits) const
 {
     // TODO
-    // Use the equations from Pre-Lab
-    vec3 u, w, v, center;
-    w = ray.endpoint;
-    u = ray.direction;
+    vec3 u;
+    vec3 v;
+    vec3 w;
+    vec3 center;
+    double a, b, c, radius;
+    //finish defs
     center = this->center;
+    u = ray.direction;
+    w = ray.endpoint;
     v = w - center;
-    double r = this->radius;
 
-    double a, b, c;
-    a = 1;
-    b = 2 * dot(u, v);
-    c = dot(v, v) - r*r;
+    radius = this->radius;
 
-    double disc = b * b - 4 * a * c;
+    a = dot(u,u);
+    b = dot(u,v);
+    c = dot(v,v) - radius*radius;
 
-    if(disc > 0) {
-        Hit hit1, hit2;
-        if(disc == 0) {
-            double t = (-b / (2 * a));
-            if(t > 0) {
-                hit1 = {this, 0, false};
-                hit2 = {this, t, true};
-                hits.push_back(hit1);
-                hits.push_back(hit2);
-            }
+    double discr = (b*b) - c;
+
+
+    if (discr > 0) {
+        double t1 = (-b - pow(discr, 0.5));
+        double t2 = (-b + pow(discr, 0.5));
+
+        if (t1 > 0) {
+            Hit hit1 = {this, t1, true};
+            hits.push_back(hit1);
         }
-        else {
-            double t1, t2;
-            t1 = (-b - pow(disc, 0.5)) / (2 * a);
-            t2 = (-b + pow(disc, 0.5)) / (2 * a);
 
-            if(t1 > 0) {
-                hit1 = {this, t1, false};
-                hits.push_back(hit1);
-            }
-            if(t2 > 0) {
-                hit2 = {this, t2, true};
-                hits.push_back(hit2);
-            }
+        if (t2 > 0) {
+            Hit hit2 = {this, t2, true};
+            hits.push_back(hit2);
         }
         return true;
     }
-
     return false;
 }
 
 vec3 Sphere::Normal(const vec3& point) const
 {
     vec3 normal;
-
     // TODO: set the normal
-    normal = (point - this->center).normalized();
-
-
+    //normal = point - center;
+    //normal.normalized()
+    normal = (point - center).normalized();
     return normal;
 }
